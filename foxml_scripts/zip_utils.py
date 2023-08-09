65c83db96238c8298bc35f4f1469ec17f5aa49aa
Hassan <hmelsheikha@gmail.com>
2023-08-09 16:41:32 -0400

    Restructuring and perfromance improvements

--- foxml_scripts/zip_utils.py
+++ foxml_scripts/zip_utils.py
@@ -1,12 +1,22 @@
 import zipfile
 import os
 import re
-import xml.etree.ElementTree as ET
 from io import BytesIO
+from typing import Optional
 
 
-def get_read_stream_zip_file(target_filename: str, zip_ref: zipfile.ZipFile) -> BytesIO:
-    """ Searches for a specific filename inside a ZipFile object (including nested zip files) and returns a read stream to the first matching file found. """
+def get_read_stream_zip_file(target_filename: str, zip_ref: zipfile.ZipFile) -> Optional[BytesIO]:
+    """
+    Search for a specified filename within a ZipFile object, including nested zip files,
+    and retrieve a read stream to the first matching file encountered.
+
+    Args:
+        target_filename (str): The filename to locate within the zip_ref.
+        zip_ref (zipfile.ZipFile): The ZipFile object to search within.
+
+    Returns:
+        Optional[BytesIO]: A read stream to the first identified matching file, or None if not found.
+    """
     for name in zip_ref.namelist():
         if re.search(r'\.zip$', name) is not None:
             # We have a nested zip within the main zip
@@ -14,36 +24,14 @@ def get_read_stream_zip_file(target_filename: str, zip_ref: zipfile.ZipFile) -> 
                 # Read the whole nested zip entry into memory
                 zfiledata = BytesIO(nested_zip_file.read())
                 with zipfile.ZipFile(zfiledata, 'r') as nested_zip_ref:
-                    nested_result = get_XML_tree_zip_file(
+                    nested_result = get_read_stream_zip_file(
                         target_filename,
                         nested_zip_ref
                     )
                 if nested_result:
+                    print('here')
                     return nested_result
         else:
             filename = os.path.basename(name)
             if target_filename in filename:
                 return zip_ref.open(name)
-
-
-def get_XML_tree_zip_file(target_filename: str, zip_ref: zipfile.ZipFile) -> ET.ElementTree:
-    """ Searches for a specific filename inside a ZipFile object (including nested zip files) and returns an ElementTree object of the first matching file found. """
-    # Get the read stream for the target file
-    target_file = get_read_stream_zip_file(target_filename, zip_ref)
-    if target_file:
-        # Return the ElementTree object for the target file
-        return ET.parse(target_file)
-
-
-def setup_namespaces():
-    """ Sets up the namespaces for the XML tree. """
-    ET.register_namespace("dc", 'http://purl.org/dc/elements/1.1/')
-    ET.register_namespace("foxml", 'info:fedora/fedora-system:def/foxml#')
-    ET.register_namespace('audit', 'info:fedora/fedora-system:def/audit#')
-    ET.register_namespace('fedora', 'info:fedora/fedora-system:def/relations-external#')
-    ET.register_namespace('fedora-model', 'info:fedora/fedora-system:def/model#')
-    ET.register_namespace('islandora', 'http://islandora.ca/ontology/relsext#')
-    ET.register_namespace('oai_dc', 'http://www.openarchives.org/OAI/2.0/oai_dc/')
-    ET.register_namespace('Policy', 'urn:oasis:names:tc:xacml:1.0:policy')
-    ET.register_namespace('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#')
-    ET.register_namespace('xsi', 'http://www.w3.org/2001/XMLSchema-instance')
